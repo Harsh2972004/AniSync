cb4f083bc313856d46b307893ebb66113e402202
Harsh <harshpreet2972004@gmail.com>
2025-05-30 18:38:16 +0530

    enabling https in server with openssl self-certification for development

--- server/app.js
+++ server/app.js
@@ -1,4 +1,10 @@
 import express from "express";
+import fs from "fs";
+import path from "path";
+import https from "https";
+import http from "http";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import session from "express-session";
 import connectDB from "./config/db.js";
 import router from "./routes/api/user.js";
@@ -7,6 +13,10 @@ import passport from "./config/passport.js";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+// Get the current directory name and file name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -36,6 +46,27 @@ app.use("/api/anime", animeRouter);
 // Connect to MongoDB
 connectDB();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.use("/", (req, res) => {
+  res.send("Welcome to the Anime API Server!");
+});
+
+//redirecting all HTTP traffic to HTTPS
+http
+  .createServer((req, res) => {
+    res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+    res.end();
+  })
+  .listen(8080);
+
+//setup ssl server
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, "cert/key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "cert/cert.pem")),
+  },
+  app
+);
+
+sslServer.listen(PORT, () => {
+  console.log(`Server is running on https://localhost:${PORT}`);
 });
