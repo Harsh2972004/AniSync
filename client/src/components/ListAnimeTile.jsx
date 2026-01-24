2d26f9b84d322276ad4919ed42bac7761122793b
Harsh <harshpreet2972004@gmail.com>
2026-01-24 18:55:01 +0530

    adding click to open to the tiles as well

--- client/src/components/ListAnimeTile.jsx
+++ client/src/components/ListAnimeTile.jsx
@@ -1,6 +1,7 @@
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 import { useUserContext } from "../context/UserListContext";
+import { Link } from "react-router-dom";
 
 const ListAnimeTile = ({ index, id, title, image }) => {
   const { animeInfo } = useUserContext();
@@ -17,23 +18,25 @@ const ListAnimeTile = ({ index, id, title, image }) => {
     cursor: "grab",
   };
   return (
-    <div
-      ref={setNodeRef}
-      {...attributes}
-      {...listeners}
-      style={style}
-      onMouseDown={(e) => (e.currentTarget.style.cursor = "grabbing")}
-      onMouseUp={(e) => (e.currentTarget.style.cursor = "grab")}
-      className="px-6 py-2 flex items-center justify-between bg-primary rounded-md touch-none"
-    >
-      <div className="flex items-center gap-4">
-        <span className="font-bold">{`${index})`}</span>
-        <img className="w-10 rounded-md" src={image} alt={`${title}-image`} />
-        <h4 className="text-sm font-semibold">{title}</h4>
-      </div>
+    <Link to={`/${id}`}>
+      <div
+        ref={setNodeRef}
+        {...attributes}
+        {...listeners}
+        style={style}
+        onMouseDown={(e) => (e.currentTarget.style.cursor = "grabbing")}
+        onMouseUp={(e) => (e.currentTarget.style.cursor = "grab")}
+        className="px-6 py-2 flex items-center justify-between bg-primary rounded-md touch-none"
+      >
+        <div className="flex items-center gap-4">
+          <span className="font-bold">{`${index})`}</span>
+          <img className="w-10 rounded-md" src={image} alt={`${title}-image`} />
+          <h4 className="text-sm font-semibold">{title}</h4>
+        </div>
 
-      <span>{score ? score : "—"}</span>
-    </div>
+        <span>{score ? score : "—"}</span>
+      </div>
+    </Link>
   );
 };
 
