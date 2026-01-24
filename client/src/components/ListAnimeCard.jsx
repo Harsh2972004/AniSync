88bbff299fd21a00a72effc15947ebad0f9f528f
Harsh <harshpreet2972004@gmail.com>
2026-01-24 18:35:54 +0530

    adding click to open anime page

--- client/src/components/ListAnimeCard.jsx
+++ client/src/components/ListAnimeCard.jsx
@@ -2,6 +2,8 @@ import { MdEdit } from "react-icons/md";
 import { FaEye } from "react-icons/fa";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import { Link } from "react-router-dom";
+
 
 const ListAnimeCard = ({
   id,
@@ -21,40 +23,42 @@ const ListAnimeCard = ({
   };
 
   return (
-    <div
-      ref={setNodeRef}
-      {...attributes}
-      {...listeners}
-      style={style}
-      onMouseDown={(e) => (e.currentTarget.style.cursor = "grabbing")}
-      onMouseUp={(e) => (e.currentTarget.style.cursor = "grab")}
-      className="w-[40vw] md:w-[20vw] xl:w-44 relative rounded-md group touch-none"
-    >
-      <img
-        className="rounded-md w-full max-h-[220px]"
-        src={image}
-        alt={title}
-      />
-      <h4 className="w-full flex items-center justify-center absolute bottom-0 text-center text-sm bg-secondary opacity-90 p-4 z-10">
-        {title}
-      </h4>
-      {list && (
-        <div className="absolute right-2 top-2 flex gap-1">
-          <button
-            onClick={onViewClick}
-            className="lg:opacity-0 group-hover:opacity-100 p-1 bg-black/70 rounded-lg transition-all duration-200"
-          >
-            <FaEye size={20} />
-          </button>
-          <button
-            onClick={onEditClick}
-            className="lg:opacity-0 group-hover:opacity-100 p-1 bg-black/70 rounded-lg transition-all duration-200"
-          >
-            <MdEdit size={20} />
-          </button>
-        </div>
-      )}
-    </div>
+    <Link ref={"/id"}>
+      <div
+        ref={setNodeRef}
+        {...attributes}
+        {...listeners}
+        style={style}
+        onMouseDown={(e) => (e.currentTarget.style.cursor = "grabbing")}
+        onMouseUp={(e) => (e.currentTarget.style.cursor = "grab")}
+        className="w-[40vw] md:w-[20vw] xl:w-44 relative rounded-md group touch-none"
+      >
+        <img
+          className="rounded-md w-full max-h-[220px]"
+          src={image}
+          alt={title}
+        />
+        <h4 className="w-full flex items-center justify-center absolute bottom-0 text-center text-sm bg-secondary opacity-90 p-4 z-10">
+          {title}
+        </h4>
+        {list && (
+          <div className="absolute right-2 top-2 flex gap-1">
+            <button
+              onClick={onViewClick}
+              className="lg:opacity-0 group-hover:opacity-100 p-1 bg-black/70 rounded-lg transition-all duration-200"
+            >
+              <FaEye size={20} />
+            </button>
+            <button
+              onClick={onEditClick}
+              className="lg:opacity-0 group-hover:opacity-100 p-1 bg-black/70 rounded-lg transition-all duration-200"
+            >
+              <MdEdit size={20} />
+            </button>
+          </div>
+        )}
+      </div>
+    </Link>
   );
 };
 
