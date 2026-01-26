ac4e2700d1bae9026c86dc227e211c209576e18e
Harsh <harshpreet2972004@gmail.com>
2026-01-26 18:56:34 +0530

    Fixing click on reorder and only view mode

--- client/src/components/ListAnimeCard.jsx
+++ client/src/components/ListAnimeCard.jsx
@@ -24,61 +24,57 @@ const ListAnimeCard = ({
     transform: CSS.Transform.toString(transform),
   };
 
-  return (
-    <Link to={`/${id}`}
-      onClickCapture={(e) => {
-        if (reorderMode) {
-          e.preventDefault();
-          e.stopPropagation();
-        }
-      }}>
-      <div
-        ref={setNodeRef}
-        {...attributes}
-        {...(reorderMode ? listeners : {})}
-        style={style}
-        className={`w-[40vw] md:w-[20vw] xl:w-44 relative rounded-md group touch-none ${reorderMode ? "touch-none " : "touch-manipulation"} ${list
-          ? "cursor-default" :
-          !reorderMode ? "cursor-default" :
-            isDragging
-              ? "cursor-grabbing"
-              : "cursor-grab"}`}
-      >
-        <img
-          className="rounded-md w-full max-h-[220px]"
-          src={image}
-          alt={title}
-        />
-        <h4 className="w-full flex items-center justify-center absolute bottom-0 text-center text-sm bg-secondary opacity-90 p-4 z-10">
-          {title}
-        </h4>
-        {list && (
-          <div className="absolute right-2 top-2 flex gap-1">
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                onViewClick?.(e);
-              }}
-              className="lg:opacity-0 group-hover:opacity-100 p-1 bg-black/70 rounded-lg transition-all duration-200"
-            >
-              <FaEye size={20} />
-            </button>
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                onEditClick?.(e);
-              }}
-              className="lg:opacity-0 group-hover:opacity-100 p-1 bg-black/70 rounded-lg transition-all duration-200"
-            >
-              <MdEdit size={20} />
-            </button>
-          </div>
-        )}
+  const CardBody = (<div
+    ref={setNodeRef}
+    {...attributes}
+    {...(reorderMode ? listeners : {})}
+    style={style}
+    className={`w-[40vw] md:w-[20vw] xl:w-44 relative rounded-md group touch-none ${reorderMode ? "touch-none " : "touch-manipulation"} ${list
+      ? "cursor-default" :
+      !reorderMode ? "cursor-default" :
+        isDragging
+          ? "cursor-grabbing"
+          : "cursor-grab"}`}
+  >
+    <img
+      className="rounded-md w-full max-h-[220px]"
+      src={image}
+      alt={title}
+    />
+    <h4 className="w-full flex items-center justify-center absolute bottom-0 text-center text-sm bg-secondary opacity-90 p-4 z-10">
+      {title}
+    </h4>
+    {list && (
+      <div className="absolute right-2 top-2 flex gap-1">
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            onViewClick?.(e);
+          }}
+          className="lg:opacity-0 group-hover:opacity-100 p-1 bg-black/70 rounded-lg transition-all duration-200"
+        >
+          <FaEye size={20} />
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            onEditClick?.(e);
+          }}
+          className="lg:opacity-0 group-hover:opacity-100 p-1 bg-black/70 rounded-lg transition-all duration-200"
+        >
+          <MdEdit size={20} />
+        </button>
       </div>
-    </Link>
-  );
+    )}
+  </div>)
+
+  if (reorderMode) {
+    return CardBody;
+  }
+
+  return <Link to={`/${id}`}>{CardBody}</Link>;
 };
 
 export default ListAnimeCard;
--- client/src/components/ListAnimeTile.jsx
+++ client/src/components/ListAnimeTile.jsx
@@ -20,42 +20,32 @@ const ListAnimeTile = ({ index, id, title, image, reorderMode }) => {
 
   const isDragging = Boolean(transform)
 
-  return (
+  const CardBody = <div
+    ref={setNodeRef}
+    {...attributes}
+    {...(reorderMode ? listeners : {})}
+    style={style}
+    onMouseDown={(e) => (e.currentTarget.style.cursor = "grabbing")}
+    onMouseUp={(e) => (e.currentTarget.style.cursor = "grab")}
+    className={`px-6 py-2 flex items-center justify-between bg-primary rounded-md ${reorderMode ? "touch-none " : "touch-manipulation"} ${!reorderMode
+      ? "cursor-default" : isDragging ? "cursor-grabbing"
+        : "cursor-grab"
+      }`}
 
-    <div
-      ref={setNodeRef}
-      {...attributes}
-      {...(reorderMode ? listeners : {})}
-      style={style}
-      onMouseDown={(e) => (e.currentTarget.style.cursor = "grabbing")}
-      onMouseUp={(e) => (e.currentTarget.style.cursor = "grab")}
-      className={`px-6 py-2 flex items-center justify-between bg-primary rounded-md ${reorderMode ? "touch-none " : "touch-manipulation"} ${!reorderMode
-        ? "cursor-default" : isDragging ? "cursor-grabbing"
-          : "cursor-grab"
-        }`}
+  >
+    <div className="flex items-center gap-4">
+      <span className="font-bold">{`${index})`}</span>
+      <img className="w-10 rounded-md" src={image} alt={`${title}-image`} />
+      <h4 className="text-sm font-semibold">{title}</h4>
+    </div>
+    <span>{score ? score : "—"}</span>
+  </div >
 
-    >
-      <div className="flex items-center gap-4">
-        <span className="font-bold">{`${index})`}</span>
-        <img className="w-10 rounded-md" src={image} alt={`${title}-image`} />
-        <h4 className="text-sm font-semibold">{title}</h4>
-      </div>
-      <div className="flex items-center justify-center gap-4">
-        <Link to={`/${id}`}
-          onClick={e => {
-            if (reorderMode) {
-              e.preventDefault()
-              e.stopPropagation()
-            }
-          }}
-          className="p-1 bg-secondary rounded-lg"
-        >
-          <FaEye size={22} />
-        </Link>
-        <span>{score ? score : "—"}</span>
-      </div>
-    </div >
-  );
+  if (reorderMode) {
+    return CardBody
+  }
+
+  return <Link to={`/${id}`}>{CardBody}</Link>
 };
 
 export default ListAnimeTile;
